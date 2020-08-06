fd05af67ecc5f352cea3377f0921d29a0f77499a
Robert <pallroberto90@gmail.com>
2020-08-06 13:01:01 +0300

    Ex.2

--- Control Structure.js
+++ Control Structure.js
@@ -3,3 +3,11 @@
 for (var i = 1; i <= 20; i++) {
     console.log(i)
 }
+
+//2.Display in the console the odd numbers from 1 to 20
+
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 == 1) {
+        console.log(i)
+    }
+}
